46303ca60b75e1fc3f7139d4077ef5860b968725
rayleigh <rayleigh190@gmail.com>
2024-12-02 20:57:55 +0900

    set: Firebase 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jnu_alarm/features/main/main_screen.dart';
+import 'package:jnu_alarm/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   // make navigation bar transparent
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
