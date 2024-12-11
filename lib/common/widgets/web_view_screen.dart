393abbc0ddcb46c19dbe7f25f11acb397c372b15
rayleigh <rayleigh190@gmail.com>
2024-12-11 21:02:41 +0900

    feat: 웹뷰의  meta, img, table 태그 조작을 위한 js 주입 구현

--- lib/common/widgets/web_view_screen.dart
+++ lib/common/widgets/web_view_screen.dart
@@ -17,10 +17,56 @@ class _WebViewScreenState extends State<WebViewScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..loadRequest(
-        Uri.parse(widget.link),
-      );
+    _controller = WebViewController();
+
+    _controller.setNavigationDelegate(
+      NavigationDelegate(onPageFinished: (String url) {
+        // 강제 확대 가능하도록 meta 태그 수정
+        _controller.runJavaScript('''
+          var meta = document.querySelector('meta[name="viewport"]');
+          if (meta) {
+            meta.setAttribute('content', 'width=device-width, initial-scale=1, user-scalable=yes');
+          } else {
+            var newMeta = document.createElement('meta');
+            newMeta.name = "viewport";
+            newMeta.content = "width=device-width, initial-scale=1, user-scalable=yes";
+            document.head.appendChild(newMeta);
+          }
+        ''');
+
+        if (widget.link.contains("jnu.ac.kr")) {
+          // 이미지 태그 크기를 100%로 변경
+          // http를 https로 변경
+          _controller.runJavaScript('''
+            document.querySelectorAll('img').forEach(img => {
+              img.style.width = '100%';
+              img.style.height = '100%';
+              if (img.src.startsWith('http://')) {
+                img.src = img.src.replace('http://', 'https://');
+              }
+            });
+          ''');
+
+          // table 태그가 화면에 넘치면 가로 스크롤 가능하도록 설정
+          _controller.runJavaScript('''
+            var tables = document.getElementsByTagName("table");
+            for (var i = 0; i < tables.length; i++) {
+              var wrapper = document.createElement("div");
+              wrapper.style.overflowX = "auto";
+              wrapper.style.width = "100%";
+              tables[i].parentNode.insertBefore(wrapper, tables[i]);
+              wrapper.appendChild(tables[i]);
+            }
+          ''');
+        }
+      }),
+    );
+
+    _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    _controller.enableZoom(true);
+    _controller.loadRequest(
+      Uri.parse(widget.link),
+    );
   }
 
   @override
