1448afba2540b0dea2f793e342bd4ac535c51c9e
rayleigh <rayleigh190@gmail.com>
2025-03-02 17:56:07 +0900

    feat: AOS알림내역 네이티브 광고를 템플릿에서 커스텀으로 변경

--- android/app/src/main/kotlin/com/example/jnu_alarm/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/jnu_alarm/MainActivity.kt
@@ -2,4 +2,22 @@ package com.jnu_alarm.android
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import com.jnu_alarm.android.NoticeNativeAdFactory
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        GoogleMobileAdsPlugin.registerNativeAdFactory(
+            flutterEngine, "NoticeNativeAd", NoticeNativeAdFactory(context)
+        )
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        super.cleanUpFlutterEngine(flutterEngine)
+
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "NoticeNativeAd")
+    }
+}
