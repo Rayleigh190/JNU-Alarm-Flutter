a49f763f4f46df765617ae975399eacee80c0741
rayleigh <rayleigh190@gmail.com>
2025-04-09 19:21:05 +0900

    refactor: 네트워크 연결 확인 패키지를 internet_connection_checker로 변경

--- lib/common/network/network_connection_check.dart
+++ lib/common/network/network_connection_check.dart
@@ -1,10 +1,10 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:jnu_alarm/common/error/exceptions/custom_exceptions.dart';
 
 Future<void> checkNetworkConnection() async {
-  final connectivityResult = await Connectivity().checkConnectivity();
-  if (!(connectivityResult.contains(ConnectivityResult.wifi) ||
-      connectivityResult.contains(ConnectivityResult.mobile))) {
+  final bool isConnected =
+      await InternetConnectionChecker.instance.hasConnection;
+  if (!isConnected) {
     throw const NetworkConnectivityException.noNetwork(
         "인터넷이 연결되어 있지 않습니다. 연결 후 재시도 해주세요.");
   }
